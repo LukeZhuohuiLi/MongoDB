481e43818fd7547e971fa89d2c52f4782b4d00d6
Zhuohui Li <L5622191423@gmail.com>
2018-09-03 21:43:46 -0700

    heroku deployment

--- HW_Week1/module01/assignment-MongoDB-Ruby-Driver-and-Rails/raceday/app/models/racer.rb
+++ HW_Week1/module01/assignment-MongoDB-Ruby-Driver-and-Rails/raceday/app/models/racer.rb
@@ -1,11 +1,12 @@
 class Racer
-  include Mongoid::Document
+   include Mongoid::Document
+   include ActiveModel::Model
   attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
   RACE_COLLECTION = 'racers'
   USERNAME = 'luke'
   PASSWORD = '123456'
 
-def initialize(params={})
+  def initialize(params={})
        @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
        @number=params[:number].to_i
        @first_name=params[:first_name]
@@ -13,7 +14,7 @@ def initialize(params={})
        @gender=params[:gender]
        @group=params[:group]
        @secs=params[:secs].to_i
-end
+  end
 
   def self.mongo_client
   	client = Mongoid::Clients.default
@@ -31,9 +32,64 @@ end
   end
 
   def self.find id
-    result = self.collection.find(:_id => id)
+    result = self.collection.find(:_id=>BSON::ObjectId.from_string(id)).first
     return result.nil? ? nil : Racer.new(result)
   end 
   
+  def save
+  	result = self.class.collection
+  	             .insert_one(number:@number,first_name:@first_name,last_name:@last_name, gender:@gender, group:group, secs:@secs)
+    @id = result.inserted_id
+  end
 
+  def update(params)
+  @number = params[:number].to_i
+  @first_name = params[:first_name]
+  @last_name = params[:last_name]
+  @secs = params[:secs].to_i
+  @gender = params[:gender]
+  @group = params[:group]
+  #delete other key and value, params will only have specified key and value.
+  params.slice!(:number, :first_name, :last_name, :gender, :group, :secs) if !params.nil?
+  self.class.collection
+            .find(:_id => BSON::ObjectId.from_string(@id))
+            .update_one(params)
+  end
+
+  def destroy
+    self.class.collection.find(:_id => BSON::ObjectId.from_string(@id)).delete_one
+  end
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def created_at
+    nil
+  end
+  def updated_at
+    nil
+  end
+
+  def self.paginate(params)
+    page = (params[:page] ||=1).to_i
+    limit = (params[:per_page] ||=30).to_i
+    skip = (page - 1)*limit
+    sort = params[:sort] ||= {:number => 1}
+    racers = []
+
+    all({},sort,skip,limit).each do |doc|
+      racers << Racer.new(doc)
+    end
+    
+      total=all({},sort,0,1).count
+      WillPaginate::Collection.create(page, limit, total) do |pager|
+      pager.replace(racers)
+      end
+  end
+
+  def to_s
+    "#{@id},number #{@number}, #{@first_name}, #{@last_name},#{@gender},#{@group},secs=#{@secs}"
+  end
+   
 end
