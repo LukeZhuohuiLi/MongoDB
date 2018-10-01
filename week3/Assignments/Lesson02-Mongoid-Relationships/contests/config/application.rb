66fe02c6c4730c50b4e04bf871fc2e46dc3b4151
Zhuohui Li <L5622191423@gmail.com>
2018-09-30 23:48:37 -0700

    HW

--- week3/Assignments/Lesson02-Mongoid-Relationships/contests/config/application.rb
+++ week3/Assignments/Lesson02-Mongoid-Relationships/contests/config/application.rb
@@ -21,6 +21,7 @@ module Contests
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    Mongoid.load!('./config/mongoid.yml')
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
