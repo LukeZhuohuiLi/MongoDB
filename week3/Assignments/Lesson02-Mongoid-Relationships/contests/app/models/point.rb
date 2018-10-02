63006eef45429ccd8b50e802b4789b42dd4e95a6
Zhuohui Li <L5622191423@gmail.com>
2018-10-01 23:39:08 -0700

    HW

---
+++ week3/Assignments/Lesson02-Mongoid-Relationships/contests/app/models/point.rb
@@ -0,0 +1,37 @@
+class Point
+  attr_accessor :longitude, :latitude
+
+  def initialize(lng, lat)
+  	@longitude = lng
+  	@latitude = lat
+  end
+
+  def mongoize
+  	{:type=> 'Point', :coordinates=>[@longitude, @latitude]}
+  end
+
+  def self.demongoize(object)
+    Point.new(object[:coordinates][0], object[:coordinates][1])
+  end
+
+  def self.mongoize(object) 
+    case object
+    when Point then object.mongoize
+    when Hash then 
+      if object[:type]  
+          Point.new(object[:coordinates][0], object[:coordinates][1]).mongoize
+      else        
+          Point.new(object[:lng], object[:lat]).mongoize
+      end
+    else object
+   end
+  end
+
+  def self.evolve(object)#same as mongoize, but this method is required by Mongoid for custom types
+    case object
+    when Point then object.mongoize
+    else object
+    end
+  end
+
+end
---
+++ week3/Assignments/Lesson02-Mongoid-Relationships/contests/app/models/racer.rb
@@ -0,0 +1,8 @@
+class Racer
+  include Mongoid::Document
+  field :first_name, as: :fn, type: String
+  field :last_name, as: :ln, type: String
+  field :date_of_birth, as: :dob, type: Date
+
+  embeds_one :primary_address, class_name: 'Address'
+end
